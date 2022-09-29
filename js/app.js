118f71fbce70626651cfc9729e67d240219846e5
Migeuskz <msanchezpapalotzi@gmail.com>
2022-09-29 12:45:48 -0500

    Leer datos del curso seleccionados

--- js/app.js
+++ js/app.js
@@ -8,12 +8,29 @@ cargarEventListener();
 function cargarEventListener(){
     //cuando un evento agrega un curso
     listaCurso.addEventListener('click', agrgarCurso);
+    console.log(e.target.parentElenment)
 }
 
 //Funciones
 function agrgarCurso(e){
     e.preventDefault();
     if(e.target.classList.contains('agregar-carrito')){
-    console.log(e.target);
+    const cursoSelecionado = e.target.parentElement.parentElement;
+    leerDatosCursos(cursoSelecionado);
     }
+}
+
+//lle el contenido del HTML al que le dimos click y extrae la información del curso
+function leerDatosCursos(curso){
+console.log(curso);
+
+//crear un objeto con el contenido del curso actual
+const infoCurso = {
+    imagen:curso.querySelector('img').src,
+    titulo:curso.querySelector('h4').textContent,
+    precio: curso.querySelector('.precio span').textContent,
+    id: curso.querySelector('a').getAttribute('data-id'),
+    cantidad: 1
+}
+console.log(infoCurso)
 }
